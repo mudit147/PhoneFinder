f7bc1d9390de267f026fc2ecc56bba28fea6d015
MUDIT MITTAL <m2mittal@uwaterloo.ca>
2021-03-10 21:03:11 +0530

    changes

--- app/src/main/java/com/example/phonefinder/PhoneFinderService.java
+++ app/src/main/java/com/example/phonefinder/PhoneFinderService.java
@@ -20,8 +20,6 @@ import androidx.annotation.RequiresApi;
 
 import java.security.Provider;
 
-import static com.example.phonefinder.MainActivity.isDeviceLocked;
-
 
 public class PhoneFinderService extends Service {
     private static final String TAG = "MainActivity";
@@ -93,6 +91,27 @@ public class PhoneFinderService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public static boolean isDeviceLocked(Context context) {
+        boolean isLocked = false;
+
+        KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        boolean inKeyguardRestrictedInputMode = keyguardManager.isKeyguardLocked();
+
+        if (inKeyguardRestrictedInputMode) {
+            isLocked = true;
+
+        } else {
+
+            PowerManager powerManager = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+                isLocked = !powerManager.isInteractive();
+            } else {
+                isLocked = !powerManager.isScreenOn();
+            }
+        }
+        return isLocked;
     }
 
     private void setAlarm(long timeInMillis) {
